6420a81c123290604d15be926254aee2cb39a89f
Oma Strachan <omastrachan91@aol.com>
2021-11-16 17:34:37 -0400

    nyc

---
+++ nyc/boroughs.py
@@ -0,0 +1,119 @@
+boroughs = {
+    'brooklyn': {
+        'beaches': {
+            'brighton beach': { 
+                'img_link': '',
+                'description': 'Also known as "Little Odessa" due its tight-knit Russian and Eastern European communities, Brooklyn\'s Brighton Beach is a lively neighborhood with many high-rise residential buildings. \ Traditional ethnic restaurants and food markets line Brighton Beach Avenue. The beach and boardwalk here are more laid-back than nearby Coney Island, catering largely to locals. Splashy nightclubs attract partiers in the evenings.'
+            },
+            'coney island': {
+                'img_link': '',
+                'description': 'Coney Island is a residential Brooklyn neighborhood that morphs into a relaxation and entertainment destination each summer. Locals and tourists crowd its beach, the Wonder Wheel and Luna Park, an amusement park featuring the famed Cyclone roller coaster. Street performers, the Circus Sideshow and the Mermaid Parade in June lend an eccentric vibe. Nathan\'s Famous is known for its July 4th hot-dog eating contest.'
+            },
+            'manhattan beach': {
+                'img_link': '',
+                'description': 'Manhattan Beach is a laid-back South Bay community popular with families and outdoor enthusiasts. The Strand bike path winds along the oceanfront between modern mansions and wide stretches of sand lined with volleyball courts. Low-key Manhattan Beach Pier features a quaint aquarium and views of surfers. There are several parks, and a small downtown area with boutiques, brunch cafes, pubs & creative, upscale eateries.'
+            }
+        }
+    },
+    'bronx': {
+        'beaches': {
+            'orchard beach': {
+                'img_link': 'https://new.mta.info/sites/default/files/2021-06/Orchard_Beach_-_Pelham_Bay_Park_-_The_Bronx.jpeg',
+                'description': 'Orchard Beach, Bronx\'s sole public beach, was proclaimed "The Riviera of New York" when it was created in the 1930s. The 115-acre, 1.1-mile-long beach contains a hexagonal-block promenade, a central pavilion, snack bars, food and souvenir carts, two playgrounds, two picnic areas, a large parking lot, and 26 courts for basketball, volleyball, and handball. Changing areas and showers are available.'
+            }
+        },
+        'stadium': {
+            'yankee stadium': {
+                'img_link': 'https://img.mlbstatic.com/mlb-images/image/private/t_16x9/t_w1536/mlb/idi1knpjdnmpqqj4qd13.jpg',
+                'description': 'Yankee Stadium is a baseball park located in Concourse, Bronx, New York City. It is the home field for the New York Yankees of Major League Baseball and New York City FC of Major League Soccer, as well as being the host stadium for the annual Pinstripe Bowl game.'
+            }
+        }
+    },
+    'manhattan': {
+        'food': {
+            'richardo steak house': {
+                'img_link': 'https://www.ricardosteakhouse.com/wp-content/uploads/2020/06/about-us-content-section-image.jpg',
+                'description': 'Ricardo Steak House. Restaurant. The ultimate expression of chic dining with an energetic urban flair.'
+            },
+            'blue sky deli': {
+                'img_link': 'https://assets3.thrillist.com/v1/image/2832644/762x508/crop;webp=auto;jpeg_quality=60.jpg',
+                'description': 'When all’s said and done, it comes down to taste, and Blue Sky Deli (formerly Hajji’s) makes the reigning champ. Chopped beef sizzles on a plancha before it’s deposited onto a hero with American cheese, peppers, onions, lettuce, tomato, ketchup, and mayo. The whole thing gets pressed and bang. Grab one to go and take it to nearby Thomas Jefferson Park to see what the fuss is about.'
+            },
+            'monkey brothers lab': {
+                'img_link': 'https://img.cdn4dd.com/p/fit=contain,width=200,height=200,format=jpeg,quality=95/media/restaurant/cover_square/7be4adf0-4925-444d-8b12-76796ae273cd.png',
+                'description': 'Food truck serving Dominican food such as Empanadas, Burgers'
+            }
+        },
+        'parks': {
+            'central park': {
+                'img_link': 'https://cdn2.civitatis.com/estados-unidos/nueva-york/guia/central-park.jpg',
+                'description': 'Central Park is an urban park in New York City located between the Upper West and Upper East Sides of Manhattan. It is the fifth-largest park in the city by area, covering 843 acres.'
+            },
+            'riverside park': {
+                'img_link': 'https://www.nycgovparks.org/photo_gallery/full_size/9736.jpg',
+                'description': 'Riverside Park is a scenic waterfront public park in the Upper West Side, Morningside Heights, and Hamilton Heights neighborhoods of the borough of Manhattan in New York City.'
+            },
+            'the highline':  {
+                'img_link': 'https://i.pinimg.com/564x/e1/50/74/e1507462634adc81f9d83ba4c29355d6.jpg',
+                'description': 'The High Line is a 1.45-mile-long elevated linear park, greenway and rail trail created on a former New York Central Railroad spur on the west side of Manhattan in New York City. The High Line\'s design is a collaboration between James Corner Field Operations, Diller Scofidio + Renfro, and Piet Oudolf.'
+            },
+            'little island':  {
+                'img_link': 'http://cdn.cnn.com/cnnnext/dam/assets/210524225200-02-little-island-ny-0525.jpg',
+                'description': 'Rising from the Hudson River, Little Island preens atop a bouquet of tulip-shaped columns, begging to be posted on Instagram. Outside, it’s eye candy. Inside, a charmer, with killer views.'
+            }
+        }
+    },
+    'queens': {
+        'airports': {
+            'john f kennedy international airport': {
+                'img_link': '',
+                'description': 'John F. Kennedy International Airport is an international airport in Queens, New York, USA. It is the primary international airport serving New York City.'
+            },
+            'laguardia airport': {
+                'img_link': '',
+                'description': 'LaGuardia Airport is an airport in Queens, New York. The airport is the third busiest airport serving New York City, and the twentieth busiest in the United States. LaGuardia Airport covers 680 acres.'
+            }
+        },
+        'beaches': {
+            'rockaway beaches': {
+                'img_link': '',
+                'description': 'From surfers to swimmers to the Ramones, everyone wants to "hitch a ride to Rockaway Beach." Rockaway Beach is a year-round resource for residents all along the Rockaway peninsula, and it comes alive each summer with millions of visitors. Beach goers can enjoy the sand and water, the variety of concessions, and the city\'s only legal surfing beaches. Rockaway Beach is also home to a variety of playgrounds and other outdoor activities.'
+            }
+        }
+
+    },
+    'staten island': {
+        'beaches': {
+            'cedar grove beach': {
+                'img_link': '',
+                'description': 'The city\'s newest beach is a converted oceanfront bungalow colony. This charming beach is smaller and somewhat more tranquil than its neighbors, Midland Beach and South Beach, and is popular with the area\'s families.'
+            },
+            'midland beach': {
+                'img_link': '',
+                'description': 'Midland Beach shares the Franklin D. Roosevelt Boardwalk with neighboring South Beach, and tends to be the somewhat less crowded of the two beaches. The beach\'s attractions include a playground and courts for tennis and shuffleball, as well as a sea turtle fountain for children.'
+            },
+            'south beach': {
+                'img_link': '',
+                'description': 'Beachgoers can sunbathe while taking in a lovely view of the Verazzano Bridge, bike through a scenic trail, jog along the boardwalk, kayak, play tennis, or fish off of the Ocean Breeze Fishing Pier, one of the city\'s most popular. A park on the beach designed especially for seniors contains chess tables, benches, and bocce courts. The beach\'s Fountain of Dolphins is a popular stop on any tour of the boardwalk.'
+            },
+            'wolfe\'s pond beach': {
+                'img_link': '',
+                'description': 'Love the ocean but think NYC’s beaches are just too chaotic? Wolfe’s Pond Beach is the city’s best-kept secret, a small, calm, and secluded beach that’s a perfect retreat for those looking to get away from summer’s crowds.'
+            }
+        },
+        'food': {
+            'lee\'s tavern': {
+                'img_link': '',
+                'description': 'Family-run neighborhood pub with photos on the wall serving a range of pizza & brews.'
+            },
+            'ralph\'s famous italian ices': {
+                'img_link': '',
+                'description': 'Staten Island-born chain dispensing Italian ices & ice cream in 100-plus flavors since 1928.'
+            },
+            'royal crown bakery': {
+                'img_link': '',
+                'description': 'A local Italian bakery serves bread, pastries & coffees, with an adjacent cucina for hot meals.'
+            }
+        }
+    },
+}
